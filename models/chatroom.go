9246e8ae286f178c607bd7c852a91db298a23230
youssefhmidi <99974719+youssefhmidi@users.noreply.github.com>
2023-08-09 11:48:57 +0100

    finished repository interface for models

--- models/chatroom.go
+++ models/chatroom.go
@@ -1,6 +1,10 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
 
 type ChatRoom struct {
 	gorm.Model
@@ -10,4 +14,23 @@ type ChatRoom struct {
 	PublicKey string
 	Members   []User    `gorm:"many2many:user_chatroom"`
 	Messages  []Message `gorm:"foreignKey:ChatRoomID"`
+}
+
+type ChatRoomRepository interface {
+	// Creat a ChatRoom
+	CreateChatRoom(ctx context.Context, room ChatRoom) error
+
+	// Get a ChatRoom
+	GetRoomByID(ctx context.Context, ID uint) (ChatRoom, error)
+	GetRoomByName(ctx context.Context, Name string) (ChatRoom, error)
+
+	// fetching multiple ChatRoom
+	GetRoomsFromUser(ctx context.Context, user User) ([]ChatRoom, error)
+	GetOwnedRooms(ctx context.Context, user User) ([]ChatRoom, error)
+
+	// Updating a ChatRoom
+	UpdateRoom(ctx context.Context, room ChatRoom) error
+
+	// Delete a ChatRoom
+	DeleteRoom(ctx context.Context, room ChatRoom) error
 }
